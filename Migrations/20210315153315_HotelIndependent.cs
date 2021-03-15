d70ce2a7fc5d907e2729bbe954ed015cff057fe2
$udam <48345668+gdsghost@users.noreply.github.com>
2021-03-15 22:07:14 +0530

    HotelModel Attributes Changed

---
+++ Migrations/20210315153315_HotelIndependent.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuggestorCodeFirstAPI.Migrations
+{
+    public partial class HotelIndependent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HotelsServices_RoomTypes_RoomTypeID",
+                table: "HotelsServices");
+
+            migrationBuilder.DropIndex(
+                name: "IX_HotelsServices_RoomTypeID",
+                table: "HotelsServices");
+
+            migrationBuilder.DropColumn(
+                name: "RoomTypeID",
+                table: "HotelsServices");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Capacity",
+                table: "HotelsServices",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomType",
+                table: "HotelsServices",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Capacity",
+                table: "HotelsServices");
+
+            migrationBuilder.DropColumn(
+                name: "RoomType",
+                table: "HotelsServices");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RoomTypeID",
+                table: "HotelsServices",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelsServices_RoomTypeID",
+                table: "HotelsServices",
+                column: "RoomTypeID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HotelsServices_RoomTypes_RoomTypeID",
+                table: "HotelsServices",
+                column: "RoomTypeID",
+                principalTable: "RoomTypes",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- Models/HotelsService.cs
+++ Models/HotelsService.cs
@@ -13,18 +13,17 @@ namespace SuggestorCodeFirstAPI.Models
         public decimal PricePerDay { get; set; }
         public string District { get; set; }
         public string Pnumber { get; set; }
+        public string RoomType { get; set; }
+        public int Capacity { get; set; }
         public string Features { get; set; }
         public string OtherDetails { get; set; }
         public string ImgURL { get; set; }
 
-        public Guid? RoomTypeID { get; set; }
-        public virtual RoomType RoomType { get; set; }
 
         public Guid? UserID { get; set; }
         public virtual User User { get; set; }
 
         public virtual List<HotelsServiceReservation> HotelsServiceReservations { get; set; }
         public virtual List<HotelsServiceComment> HotelsServiceComments { get; set; }
-        public virtual List<HotelServiceRoomType> HotelServiceRoomTypes { get; set; }
     }
 }
